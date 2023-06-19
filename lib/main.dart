36b0e8d0e8fad90abf3051e223a1dec3d433cc29
rmeht <r.mehtabd89@gmail.com>
2023-06-19 14:50:37 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,265 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductList(),
+    );
+  }
+}
+
+class ProductList extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product(name: 'Product 1', price: 10),
+    Product(name: 'Product 2', price: 20),
+    Product(name: 'Product 3', price: 30),
+    Product(name: 'Product 4', price: 40),
+    Product(name: 'Product 5', price: 50),
+    Product(name: 'Product 6', price: 60),
+    Product(name: 'Product 7', price: 70),
+    Product(name: 'Product 8', price: 80),
+    Product(name: 'Product 9', price: 90),
+    Product(name: 'Product 10', price: 100),
+    Product(name: 'Product 11', price: 110),
+    Product(name: 'Product 12', price: 120),
+    // Add more products here
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CartPage(products)),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            subtitle: Text('Price: \$${products[index].price}'),
+            trailing: CounterButton(
+              product: products[index],
+              onPressed: () {
+                setState(() {
+                  products[index].incrementCounter();
+                  if (products[index].counter == 5) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Congratulations!'),
+                          content:
+                          Text('You\'ve bought 5 ${products[index].name}!'),
+                          actions: [
+                            TextButton(
+                              child: Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CounterButton extends StatelessWidget {
+  final Product product;
+  final VoidCallback onPressed;
+
+  CounterButton({required this.product, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text('Buy Now (${product.counter})'),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class Product {
+  final String name;
+  final double price;
+  int counter;
+
+  Product({required this.name, required this.price, this.counter = 0});
+
+  void incrementCounter() {
+    counter++;
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+
+  CartPage(this.products);
+
+  @override
+  Widget build(BuildContext context) {
+    int totalBought = products.fold(0, (sum, product) => sum + product.counter);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Text('Total products bought: $totalBought'),
+      ),
+    );
+  }
+}
+
+
+
+/**import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PhotoGalleryApp());
+}
+
+class PhotoGalleryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PhotoGalleryScreen(),
+    );
+  }
+}
+
+class PhotoGalleryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to My Photo Gallery!',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for photos',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+            ),
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Clicked on photo!')),
+                    );
+                  },
+                  child: Column(
+                    children: [
+                      Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      Image.network(
+                        'hb6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 8.0),
+                      Text('Photo 1'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ListView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                ListTile(
+                  leading: Icon(Icons.photo),
+                  title: Text('Photo Title 1'),
+                  subtitle: Text('Photo Subtitle 1'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Photos Uploaded Successfully!')),
+          );
+        },
+        child: Icon(Icons.cloud_upload),
+      ),
+    );
+  }
+}
+
+**/
+
+
+
+
+
+
+
+/**import 'package:flutter/material.dart';
+
+void main() {
   runApp(ItemListApp());
 }
 
@@ -53,4 +312,4 @@ class ItemListScreen extends StatelessWidget {
       ),
     );
   }
-}
+}**/
